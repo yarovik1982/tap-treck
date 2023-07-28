ff124fc662c5e93e6b5099a310449c9b8cfdf238
acunamatata82 <yar.oleg1982@mail.ru>
2023-07-28 14:04:31 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<h1>Тестовый запуск</h1>
+  <nav>
+    <button class="btn btn-primary btn-sm" @click="toggleEye">
+      <i class="bi bi-eye-slash" v-if="isToggle"></i>
+      <i class="bi bi-eye" v-else></i>
+    </button>
+  </nav>
+  <!-- <router-view/> -->
+</template>
+<script>
+import {ref} from 'vue'
+export default {
+  setup() {
+    const isToggle = ref(false)
+    const toggleEye = () => {
+      isToggle.value = !isToggle.value
+    }
+    return{
+      isToggle,
+      toggleEye
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router/router'
+import store from './store/store'
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const app = createApp(App)
+
+app
+.use(store)
+.use(router)
+.mount('#app')
